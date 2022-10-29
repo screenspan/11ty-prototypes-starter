18f563678c25daca026af007c63cd6da5c018ad1
Brian Louis Ramirez <19509110+screenspan@users.noreply.github.com>
2022-10-29 14:45:12 +0200

    restructure some _src directories

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,19 @@
+const CleanCSS = require("clean-css");
 const { minify } = require("terser");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("_src/_images/*");
+  eleventyConfig.addPassthroughCopy({ "_src/_data/*": "data/" });
+  eleventyConfig.addPassthroughCopy({ "_src/_includes/css/*.css": "css/" });
+  eleventyConfig.addPassthroughCopy({ "_src/_favicon/*": "./" });
+  eleventyConfig.addPassthroughCopy("_src/prototypes/**/*.css");
+  eleventyConfig.addPassthroughCopy("_src/prototypes/**/*.js");
+  eleventyConfig.addPassthroughCopy("_src/prototypes/**/*.json");
+  eleventyConfig.addPassthroughCopy("_src/prototypes/**/*.png");
 
-  eleventyConfig.addPassthroughCopy({ "_src/_images/favicon/*": "./" });
-
-  eleventyConfig.setLiquidOptions({
-    // dynamicPartials: false,
-    strictFilters: false, // renamed from `strict_filters` in Eleventy 1.0
-  });
-
-  eleventyConfig.addFilter("jsmin", async function(code) {
+  eleventyConfig.addFilter("jsmin", async function (code) {
     try {
-      let options = { toplevel: true };
-      const minified = await minify(code, options);
+      const minified = await minify(code);
       console.log(`Minified JS.`);
       return minified.code;
     } catch (err) {
@@ -21,6 +22,18 @@ module.exports = function (eleventyConfig) {
       // Fail gracefully.
       return code;
     }
+  });
+
+  eleventyConfig.addFilter("cssmin", function (code) {
+    let cleanedCSS = new CleanCSS({}).minify(code);
+    let minifiedCSSSize = cleanedCSS.stats.minifiedSize / 1000;
+    console.log("Minified CSS: " + minifiedCSSSize + " kb");
+    return cleanedCSS.styles;
+  });
+
+  eleventyConfig.setLiquidOptions({
+    // dynamicPartials: false,
+    strictFilters: false, // renamed from `strict_filters` in Eleventy 1.0
   });
 
   return {
